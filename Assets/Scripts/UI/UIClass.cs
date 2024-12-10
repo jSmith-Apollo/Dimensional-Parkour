b3df65635da7aca54141394e276fad0fc17e67ba
JWyatt <Jackson.wyatt@stu.daviess.kyschools.us>
2024-12-10 12:56:04 -0600

    Finished UIClass

--- Assets/Scripts/UI/UIClass.cs
+++ Assets/Scripts/UI/UIClass.cs
@@ -8,7 +8,25 @@ public class UIClass : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        /* Check if descendants and children work (Works) //
+        print("Getting Descendants");
+        GameObject[] descendants = getDescendants(transform.gameObject);
+        for (int i = 0; i < descendants.Length; i++)
+        {
+            print("[" + i + "] " + descendants[i].name + "\n");
+        }
+        print("-------------------");
+        print("Getting Children");
+        GameObject[] children = getChildren(transform.gameObject);
+        for (int i = 0; i < children.Length; i++)
+        {
+            print("[" + i + "] " + children[i].name + "\n");
+        }
+        */
+        /* Negate Negates the image and unNegates when ran again //
+        Negate();
+        Negate();
+        */
     }
 
     // Update is called once per frame
@@ -32,8 +50,8 @@ public class UIClass : MonoBehaviour
         }
     }
 
-    //Get all objects direct children in the hierarcy//
-    private GameObject[] getChildren(GameObject obj)
+    //Get all objects descendants in the hierarcy//
+    public GameObject[] getDescendants(GameObject obj)
     {
         //return null if no object is present
         if (obj == null) return null;
@@ -50,8 +68,8 @@ public class UIClass : MonoBehaviour
         return children;
     }
 
-    //Get all objects Descendants in the hierarcy//
-    private GameObject[] getDescendants(GameObject obj)
+    //Get all objects main children in the hierarcy//
+    public GameObject[] getChildren(GameObject obj)
     {
         //return null if no object is present
         if (obj == null) return null;
@@ -80,15 +98,11 @@ public class UIClass : MonoBehaviour
         obj.GetComponent<Image>().color = new Color(r, g, b, a);
     }
 
-    private void visibleState(float state)
+    private void visibleState(bool state)
     {
-        //Get all descendants of the UIClass//
-        GameObject[] Descendants = getDescendants(transform.gameObject);
-        //For every object 
-        foreach (GameObject obj in Descendants)
-        {
-            setColor(obj, obj.GetComponent<Image>().color.r, obj.GetComponent<Image>().color.g, obj.GetComponent<Image>().color.b, state);
-        }
+        if (transform.gameObject.GetComponent<Canvas>() == null) return;
+
+        transform.gameObject.GetComponent<Canvas>().enabled = state;
     }
     
     //Change the scale of a deminsion to the a value between 0-1//
@@ -109,6 +123,21 @@ public class UIClass : MonoBehaviour
         else if (Dem == 'z')
         {
             obj.transform.localScale = new Vector3(obj.transform.localScale.x, obj.transform.localScale.y, val);
+        }
+    }
+
+    public void Negate()
+    {
+        GameObject[] canvasDescendants = getDescendants(transform.gameObject);
+        foreach (GameObject obj in canvasDescendants)
+        {
+            if (obj.GetComponent<Image>() != null)
+            {
+                float trans = obj.GetComponent<Image>().color.a;
+                Color.RGBToHSV(obj.GetComponent<Image>().color, out float H, out float S, out float V);
+                float negH = (H + 0.5f) % 1f;
+                obj.GetComponent<Image>().color = new Color(Color.HSVToRGB(negH,S,V).r, Color.HSVToRGB(negH, S, V).g, Color.HSVToRGB(negH, S, V).b,trans);
+            }
         }
     }
 }
